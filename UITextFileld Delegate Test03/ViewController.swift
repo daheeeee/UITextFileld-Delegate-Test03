33b879ccf6ea30420e2ffd3ec466ea65edaa91e3
D7703_18 <d7703_18@d7703-18ui-imac.local>
2018-03-22 15:46:36 +0900

    add background

--- UITextFileld Delegate Test03/ViewController.swift
+++ UITextFileld Delegate Test03/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lab: UILabel!
+    
+    @IBOutlet weak var txt: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        txt.placeholder = "입력을 하세요"     //TextField 입력하시요 코딩
+        txt.clearButtonMode = UITextFieldViewMode.whileEditing
+        txt.borderStyle = UITextBorderStyle.line    //TextField의 라인을 진하게 함
     }
 
+    @IBAction func but(_ sender: Any) {
+        lab.text = "Hello" + txt.text!
+        txt.text = ""
+        
+        //키패드를. 내림
+            txt.resignFirstResponder()
+    }
+    
+    //background의 View를 누르면 keybord가 내려감
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
-}
+ }
+
 
